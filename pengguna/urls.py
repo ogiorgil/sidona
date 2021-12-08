0ca5ce06bfdc3ba7db897ee1e1cb58a515d7daea
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2021-12-08 22:14:37 +0700

    Implement login

--- pengguna/urls.py
+++ pengguna/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("login/", views.login),
-    path("register/", views.register),
+    path("register/", views.register_view),
     path("profil/", views.read_pengguna),
     path("daftar_pengguna/", views.tabel_daftar_pengguna),
     path("admin_detail_pengguna/", views.admin_pov_read_pengguna),
--- pengguna/views.py
+++ pengguna/views.py
@@ -1,12 +1,73 @@
-from django.shortcuts import render
 from django.http import JsonResponse
+from django.shortcuts import redirect, render
+from django.views.decorators.csrf import csrf_exempt
+
+from utils.query import query
 
 
+def is_authenticated(request):
+    try:
+        request.session['email']
+        return True
+    except KeyError:
+        return False
+
+
+def get_role(email, password):
+    if len(query(f"SELECT 1 FROM ADMIN WHERE email = '{email}' AND password = '{password}'")):
+        return "admin"
+
+    if len(query(f"SELECT 1 FROM PENGGALANG_DANA WHERE email = '{email}' AND password = '{password}'")):
+        if len(query(f"SELECT 1 FROM INDIVIDU WHERE email = '{email}'")):
+            return "individu"
+        else:
+            return "organisasi"
+
+    return ""
+
+
+@csrf_exempt
 def login(request):
+    next = request.GET.get('next')
+
+    if request.method != "POST":
+        return login_view(request)
+
+    try:
+        email = str(request.session['email'])
+        password = str(request.session['password'])
+    except:
+        email = str(request.POST['email'])
+        password = str(request.POST['password'])
+
+    role = get_role(email, password)
+    print(role)
+
+    if (role == ""):
+        return login_view(request)
+    else:
+        request.session['email'] = email
+        request.session['password'] = password
+        request.session['role'] = role
+
+        if (next != None and next != "None"):
+            return redirect(next)
+        else:
+            return redirect("/t3/daftar-penggalangan")
+
+
+def login_view(request):
     return render(request, "login.html")
 
 
-def register(request):
+def login_view(request):
+    if is_authenticated(request):
+        return redirect("/t3/daftar-penggalangan")
+
+    return render(request, "login.html")
+
+
+def register_view(request):
     return render(request, "register.html")
 
 
