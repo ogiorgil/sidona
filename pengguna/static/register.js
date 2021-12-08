93fe5aa6c470239e65e0ff574a07b0be28226559
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2021-12-08 23:55:52 +0700

    Co-authored-by: David Alexander <david-alexander01@users.noreply.github.com>

--- pengguna/static/register.js
+++ pengguna/static/register.js
@@ -1,5 +1,37 @@
 let jenisAkun, jenisPd;
 
+const setAdminRequired = (isRequired) => {
+  $("#adminEmailInput").prop("required", isRequired);
+  $("#adminPasswordInput").prop("required", isRequired);
+  $("#adminNamaInput").prop("required", isRequired);
+  $("#adminNoHpInput").prop("required", isRequired);
+};
+
+const setPdRequired = (isRequired) => {
+  $("#pdEmailInput").prop("required", isRequired);
+  $("#pdPasswordInput").prop("required", isRequired);
+  $("#pdNamaInput").prop("required", isRequired);
+  $("#pdNoHpInput").prop("required", isRequired);
+  $("#pdAlamatInput").prop("required", isRequired);
+  $("#pdNamaBankInput").prop("required", isRequired);
+  $("#pdNomorRekeningInput").prop("required", isRequired);
+}
+
+const setIndividuRequired = (isRequired) => {
+  $("#pdiNikInput").prop("required", isRequired);
+  $("#pdiTanggalLahirInput").prop("required", isRequired);
+  $("#pdiJenisKelaminInput").prop("required", isRequired);
+  $("#pdiKtpInput").prop("required", isRequired);
+};
+
+const setOrganisasiRequired = (isRequired) => {
+  $("#pdoNamaInput").prop("required", isRequired);
+  $("#pdoNoTelpInput").prop("required", isRequired);
+  $("#pdoTahunInput").prop("required", isRequired);
+  $("#pdoNomorAktaInput").prop("required", isRequired);
+  $("#pdoFotoAktaInput").prop("required", isRequired);
+};
+
 $(document).ready(() => {
   // Hide unused form sections.
   $("#adminSection").hide();
@@ -15,11 +47,21 @@ $(document).ready(() => {
       $("#pdiSection").hide();
       $("#pdoSection").hide();
       $("#adminSection").show();
+
+      setPdRequired(false);
+      setIndividuRequired(false);
+      setOrganisasiRequired(false);
+      setAdminRequired(true);
     } else if (jenisAkun === "Penggalang Dana") {
       $("#adminSection").hide();
       $("#pdiSection").hide();
       $("#pdoSection").hide();
       $("#pdSection").show();
+
+      setAdminRequired(false);
+      setIndividuRequired(false);
+      setOrganisasiRequired(false);
+      setPdRequired(true);
     }
   });
 
@@ -29,9 +71,19 @@ $(document).ready(() => {
     if (jenisPd === "Individu") {
       $("#pdoSection").hide();
       $("#pdiSection").show();
+
+      setAdminRequired(false);
+      setOrganisasiRequired(false);
+      setPdRequired(true);
+      setIndividuRequired(true);
     } else if (jenisPd === "Organisasi") {
       $("#pdiSection").hide();
       $("#pdoSection").show();
+
+      setAdminRequired(false);
+      setIndividuRequired(false);
+      setPdRequired(true);
+      setOrganisasiRequired(true);
     }
   });
 });
--- pengguna/urls.py
+++ pengguna/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("login/", views.login),
     path("logout/", views.logout),
-    path("register/", views.register_view),
+    path("register/", views.register),
     path("profil/", views.read_pengguna),
     path("daftar_pengguna/", views.tabel_daftar_pengguna),
     path("admin_detail_pengguna/", views.admin_pov_read_pengguna),
