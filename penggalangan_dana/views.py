d0dbff7cc369cf9440fdbf9295ad72b3a2b34ecc
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2021-12-10 05:08:00 +0700

    Implement penggalangan dana details

--- penggalangan_dana/views.py
+++ penggalangan_dana/views.py
@@ -26,9 +26,67 @@ def read_pd(request):
     """)
 
     data = {"penggalangan_dana": penggalangan_dana_pribadi}
-    print(data)
     return render(request, "read_pd.html", data)
 
 
 def read_pd_details(request):
-    return render(request, "read_pd_details.html")
+    id = request.GET.get("id", "")
+
+    # No penggalangan dana id provided.
+    if not id:
+        return redirect("/pd")
+
+    if not is_authenticated(request):
+        return redirect(f"/auth/login?next=/pd/details/?id={id}")
+
+    session = get_session_data(request)
+    if session.get("role", "") not in ["admin", "individu", "organisasi"]:
+        return HttpResponse("You are not authorized!")
+
+    penggalangan_dana = query(f"""
+        SELECT * 
+        FROM penggalangan_dana_pd AS pdpd, (
+            SELECT id AS kategori_id, namakategori FROM kategori_pd
+        ) AS k
+        WHERE pdpd.id = '{id}' AND pdpd.idkategori = k.kategori_id
+    """)
+
+    if len(penggalangan_dana) == 0:
+        return HttpResponse("Penggalangan Dana does not exists!")
+
+    penggalangan_dana = penggalangan_dana[0]
+    data = {"penggalangan_dana": penggalangan_dana}
+
+    if penggalangan_dana.namakategori == "Kesehatan":
+        pasien = query(f"""
+            SELECT * 
+            FROM penggalangan_dana_pd AS pdpd 
+            INNER JOIN pd_kesehatan AS pdk ON pdpd.id = pdk.idpd
+            INNER JOIN pasien AS p ON pdk.idpasien = p.nik
+        """)
+
+        if len(pasien) == 0:
+            return HttpResponse("Pasien does not exists!")
+
+        data["pasien"] = pasien[0]
+    elif penggalangan_dana.namakategori == "Rumah Ibadah":
+        rumah_ibadah = query(f"""
+            SELECT *
+            FROM penggalangan_dana_pd AS pdpd
+            INNER JOIN pd_rumah_ibadah AS pdri ON pdpd.id = pdri.idpd
+            INNER JOIN rumah_ibadah AS ri ON pdri.idrumahibadah = ri.nosertifikat
+            INNER JOIN (
+                SELECT id AS aktivitas_id, nama AS aktivitasnama
+                FROM kategori_aktivitas_pd_rumah_ibadah
+            ) AS ka ON pdri.idaktivitas = ka.aktivitas_id
+            INNER JOIN (
+                SELECT idpd AS komorbid_idpd FROM komorbid
+            ) AS ko ON pdpd.id = ko.komorbid_idpd
+        """)
+
+        if len(rumah_ibadah) == 0:
+            return HttpResponse("Rumah Ibadah does not exitsts!")
+
+        data["rumah_ibadah"] = rumah_ibadah[0]
+
+    return render(request, "read_pd_details.html", data)
