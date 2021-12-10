bdc282b0246a94d6d9fdbb2f56b8ab1db4581193
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2021-12-10 23:33:28 +0700

    Add delete penggalangan dana

--- penggalangan_dana/urls.py
+++ penggalangan_dana/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.read_pd, name="read"),
     path("create/", views.create_pd, name="create"),
     path("details/", views.read_pd_details, name="read_details"),
+    path("delete/", views.delete_pd, name="delete"),
 
     path("kategori/", views.get_kategori, name="get_kategori"),
     path("generate-id/", views.generate_id, name="generate_id"),
